72d5ccc7eec413bf836b927e503ee9b9a813c3b3
Tsiry <hei.tsiry@gmail.com>
2026-04-27 22:51:57 +0300

    feat: add reservation class

---
+++ AgriConnect/src/main/java/agriconnect/mada/agriconnect/repository/ReservationRepository.java
@@ -0,0 +1,13 @@
+package agriconnect.mada.agriconnect.repository;
+
+import agriconnect.mada.agriconnect.entity.Reservation;
+
+import java.util.List;
+
+public class ReservationRepository {
+    public List<Reservation> getAllReservations(){throw new RuntimeException("Not Implemented");}
+    public Reservation saveReservation(Reservation reservation){throw new RuntimeException("Not Implemented");}
+    public Reservation getReservationByID(int reservationID){throw new RuntimeException("Not Implemented");}
+    public void deleteReservationByID(int reservationID){throw new RuntimeException("Not Implemented");}
+    public Reservation updateReservation(Reservation reservation){throw new RuntimeException("Not Implemented");}
+}
