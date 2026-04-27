f09feb5e216890331f8391675185de287195545d
Tsiry <hei.tsiry@gmail.com>
2026-04-27 22:17:03 +0300

    feat: add db.sql

---
+++ AgriConnect/src/main/resources/sql/db.sql
@@ -0,0 +1,14 @@
+\c postgres
+CREATE database agri_connect_db;
+
+CREATE user agri_connect_db_manager with password '123456';
+
+grant connect on database agri_connect_db to agri_connect_db_manager;
+
+\c agri_connect_db
+
+grant create, usage on schema public to agri_connect_db_manager;
+alter default privileges in schema public
+      grant select, update, insert, delete on tables to agri_connect_db_manager;
+alter default privilages in schema public
+      grant usage, select, update on sequences to agri_connect_db_manager;
