16829a86cbd60994f0750e71451db907f0e378f9
Tojoniaina Randrianasoa <tojoniaina@gmail.com>
2026-04-27 21:06:46 +0300

    feat: add structure backend project

---
+++ AgriConnect/src/test/java/agriconnect/mada/agriconnect/AgriConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package agriconnect.mada.agriconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriConnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
